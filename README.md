9b595d7e2d0c41e38e33285b75baee6aa885781e
Austin Somaduroff <ajsomaduroff@alaska.edu>
2025-03-08 13:50:48 -0900

    Begin Analysis, Upload Files

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # State of Alaska Data Analysis
  This is where I explore State of Alaska data I can find.
+
+
+## 2024 General Election - November 5, 2024
+
+Source: https://www.elections.alaska.gov/election-results/e/?id=24genr
+
+Files: State of Alaska Data Analysis/data/General Election 2024/CVR_Export_20241130154411.zip
+
+Date Accessed: 2025-03-08
+
+Page Last Updated: 2025-01-09 2:58PM
+
+### Cast Vote Record 
+
+The following is stated on the Source webpage
+
+The Cast Vote Record (CVR) contains the votes and rankings on all the election ballots that were scanned. It does not include ballots that were only counted by hand. It also does not indicate the rankings that each individual write-in candidate received. The CVR shows how voters ranked write-in candidates, but it does not show the names the voters wrote in.
+
+The CVR is a JSON file, used by the ranked-choice software to tabulate the results. The CVR is not tabulation, it is a record of ballots. The division is providing the CVR to increase transparency and provide voters the data necessary to confirm the results. The division cannot help voters access or analyze the data.
---
+++ State of Alaska Data Analysis/code/General Election 2024.R
@@ -0,0 +1,85 @@
+# PURPOSE #####################################################################
+# Analyst: Austin Somaduroff
+# Project: State of Alaska Data Analysis
+# Title: General Election 2024.R
+
+# README ######################################################################
+
+# I'm using this area to learn about the General Election Data available from
+# the State of Alaska, Division of Elections website.
+
+# The files are JSON.
+
+
+
+# SETUP #######################################################################
+
+library(tidyverse)
+library(jsonlite)
+
+# IMPORT ######################################################################
+
+files_and_paths <- tibble(
+  filenames = list.files(path = 'data/General Election 2024/CVR_Export_20241130154411/'),
+  filepaths = list.files(path = 'data/General Election 2024/CVR_Export_20241130154411/',
+                         full.names = TRUE)) |> 
+  mutate(cvr = str_detect(filenames, pattern = 'Cvr'))
+
+
+
+
+cvr_test <- fromJSON(txt = 'data/General Election 2024/CVR_Export_20241130154411/CvrExport_100.json')
+
+ballot_type_contest_manifest <- fromJSON('data/General Election 2024/CVR_Export_20241130154411/BallotTypeContestManifest.json') |> 
+  as.data.frame()
+
+files_and_paths$filenames
+files_and_paths$filepaths
+
+f_json_to_df <- function(x) {
+  x$filename <- as.data.frame(fromJSON(txt = toString(x$filepaths)))
+}
+
+f_json_to_df_test <- function(x) {
+  
+  for (filename in list) {
+    print(x$filename)
+    print(toString(x$filepaths))
+    }
+}
+
+x
+
+files_and_paths |>
+  filter(!cvr) |> 
+  f_json_to_df_test()
+
+
+files_and_paths |> 
+  filter(!cvr) |> 
+  slice_head(n = 1) |> 
+  select(filepaths) |> 
+  toString() |> 
+  fromJSON()
+
+files_and_paths |> 
+  filter(!cvr) |> 
+  f_json_to_df()
+
+# REVIEW ######################################################################
+
+# CLEAN / MUTATE ##############################################################
+
+## Add New Fields =============================================================
+
+## Remove Error Records =======================================================
+
+## Replace NA Values ==========================================================
+
+# ANALYZE #####################################################################
+
+## First Thing ================================================================
+
+## Second Thing ===============================================================
+
+### First part of second thing ------------------------------------------------
